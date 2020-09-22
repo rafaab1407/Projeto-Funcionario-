8feeee6c9b6640e04b7490d1efe008412b31284b
Rafael Borges <rafael.borges1407@gmail.com>
2020-09-22 17:56:23 -0300

    Mudancas no testa Funcionario

--- testaFuncionario.java
+++ testaFuncionario.java
@@ -1,7 +1,7 @@
 public class testaFuncionario {
     public static void main(String[] args) {
         Funcionario funcionario = new Gerente();
-        funcionario.setNome("Rafael");
+        funcionario.setNome("Rafael Bprges");
         funcionario.setCpf("144.918.166.03");  
         funcionario.setSalario(2000);
 
