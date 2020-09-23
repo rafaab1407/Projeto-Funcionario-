60cf928d747c42a1221f21611043dca5c32203ff
Rafael Borges <rafael.borges1407@gmail.com>
2020-09-23 01:40:25 -0300

    teste commit

--- testaFuncionario.java
+++ testaFuncionario.java
@@ -4,7 +4,8 @@ public class testaFuncionario {
         funcionario.setNome("Rafael Bprges");
         funcionario.setCpf("144.918.166.03");  
         funcionario.setSalario(2000);
-
+   
         System.out.println(funcionario.getBonificacao());
+        System.out.println("teste branch");
     }//fim main
 }//fim class
